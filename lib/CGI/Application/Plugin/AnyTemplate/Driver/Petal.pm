73eea319200242a4b87c5fecd90e8bd94a876a0d
Michael Graham <michael@caliope.(none)>
2009-10-18 14:27:17 -0400

    Removed spurious call to $template->error in Petal driver.

--- lib/CGI/Application/Plugin/AnyTemplate/Driver/Petal.pm
+++ lib/CGI/Application/Plugin/AnyTemplate/Driver/Petal.pm
@@ -220,7 +220,7 @@ sub render_template {
     my $params = $self->get_param_hash;
     $params->{$driver_config->{'embed_tag_name'}} = $component_handler;
 
-    my $output = $template->process($params) || croak $template->error;
+    my $output = $template->process($params);
     return \$output;
 }
 
